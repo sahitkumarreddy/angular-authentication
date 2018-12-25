45fecbcf0ca137cc767ccc898e55616642697270
Sahit Kumar Reddy <sahitkumarreddy@gmail.com>
2018-12-25 15:09:01 +0530

    user.js file change

--- server/models/user.js
+++ server/models/user.js
@@ -2,21 +2,10 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-var validateEmail = function(email) {
-
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-    return re.test(email)
-
-};
-
 const userSchema = new Schema({
-    email:    { 
+    email:  { 
         type: String,     
-        Required:  'Email address cannot be left blank.',    
-        validate: [validateEmail, 'Please fill a valid email address'],
-             match: [pattern="[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}", 'Please fill a valid email address'],
-        index: {unique: true, dropDups: true}       
+        Required:  'Email address cannot be left blank.'     
         },
 
     password: { type: String , required: [true,  'Password cannot be left blank']}
